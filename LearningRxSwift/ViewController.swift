50a4ccbeaec686d8f8906d6e78c311afde6eec6c
Nada <nada.mostafa9@gmail.com>
2021-10-12 22:15:09 +0200

    adding tableView

--- LearningRxSwift/ViewController.swift
+++ LearningRxSwift/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let items = ["item1", "item2", "item3"]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+}
