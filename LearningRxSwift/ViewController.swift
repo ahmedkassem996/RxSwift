dcab1f761f92135849575d1ffbac48f0ed183004
Nada <nada.mostafa9@gmail.com>
2021-10-12 22:36:19 +0200

    hellow RX

--- LearningRxSwift/ViewController.swift
+++ LearningRxSwift/ViewController.swift
@@ -7,33 +7,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
-    let items = ["item1", "item2", "item3"]
+    let items = Observable.just(["item1", "item2", "item3"])
+        let disposeBag = DisposeBag()
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-
-
-}
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = items[indexPath.row]
         
-        return cell
+        items.bind(to: tableView.rx.items(cellIdentifier: "myCell")) {tv , tableViewItems, cell in
+            cell.textLabel?.text = tableViewItems
+        }.disposed(by: disposeBag)
     }
+
+
 }
